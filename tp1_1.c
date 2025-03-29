624ca2569d52280c8641902e521780c8642922f8
“Agust1nSc” <agustinsaccone9@gmail.com>
2025-03-29 10:21:07 -0300

    Primera parte de C

--- tp1_1.c
+++ tp1_1.c
@@ -2,7 +2,17 @@
 
 int main(){
 
-printf("Hola mundo");
+printf("Hola mundo\n");
+
+int variable = 5;
+int  *p = &variable;
+
+
+printf("El contenido del puntero es: %d\n", *p);
+printf("La direccion de memoria almacenada por el puntero: %p\n", variable);
+printf("La direccion de memoria de la variable: %p\n", &variable);
+printf("La direccion de del puntero: %p\n", &p);
+printf("Tamano de memoria usado por la variable: %d\n", sizeof(variable));
 
     return 0;
 }
